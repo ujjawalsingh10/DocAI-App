3e07c92afb62490d80e81cd94c6db5bca9c172c2
ujjawalsingh10 <iamujjawal10.us@gmail.com>
2023-06-17 00:32:06 +0530

    Creating bounding boxes using easyOCR

--- docAI_app.py
+++ docAI_app.py
@@ -1,7 +1,32 @@
-import streamlit as st
+# import streamlit as st
 
-header = st.container()
+# header = st.container()
 
-with header:
-    st.title("Title not decided yet")
-    st.text('Read pdf and perform ocr')
+# with header:
+#     st.title("Title not decided yet")
+#     st.text('Read pdf and perform ocr')
+
+from PIL import Image
+import easyocr
+import cv2 as cv
+
+img = cv.imread('invoice.png')
+
+##OCR reader
+read = easyocr.Reader(['en'])
+
+## Performing OCR on image
+ocr_image = read.readtext(img)
+
+## Bounding boxes
+for detection in ocr_image:
+    box, text, score = detection
+    x_min, y_min = map(int, box[0])
+    x_max, y_max = map(int, box[2])
+
+    ## Creating Red colored Bounding box
+    cv.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+
+print('done')
+cv.imwrite('Invoice_OCRd.png', img)
+        
